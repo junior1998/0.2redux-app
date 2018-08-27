09927646e743a6fbeaedfe57601488688342493e
junior1998 <isabelwiliams1@gmail.com>
2018-08-26 21:47:53 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '../../node_modules/@ngrx/store';
+import { Action } from '@ngrx/store';
+
+interface AppStore{
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+   contador: number;
+
+   constructor(private store: Store<AppStore>) {
+     this.store.subscribe( state =>{
+       this.contador = state.contador
+     })
+    //  this.contador = 10;
+   }
+
+   incrementar() {
+    //  this.contador ++;
+
+    const accion: Action = {
+      type: 'INCREMENTAR'
+    };
+
+    this.store.dispatch(accion);
+   }
+
+   decrementar() {
+    //  this.contador --;
+
+    const accion: Action = {
+      type: 'DECREMENTAR'
+    };
+
+    this.store.dispatch(accion);
+
+   }
 }
